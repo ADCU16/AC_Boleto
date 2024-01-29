612792093b21a93792f05ea5409daa884ab23543
ADCU16 <adolfo.cerau@pca.edu.co>
2024-01-29 09:55:43 -0500

    Creación del home e intro

---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,68 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage-angular';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: 'intro.page.html',
+  styleUrls: ['intro.page.scss'],
+})
+export class IntroPage {
+
+  slides = [
+    {
+      title: "Hola, Bienvenido",
+      classDescription:"descripcionTexto",
+      description: "¡Gracias por instalar la app,recuerda que con nosotrostienes muchó más!", 
+      Image: "../../assets/gif/techny-email-marketing-and-newsletter.gif",
+      Button: ""
+    },
+    {
+      title: "Seguridad",
+      classDescription:"descripcionTexto",
+      description: "Tranquilo, con nosotros túscompras están encriptadas ysiempre son seguras.",
+      Image: "../../assets/gif/techny-secure-lock-and-key-successfully-unlocked.gif",
+      Button: ""
+    },
+    {
+      title: "Compras internacionales",
+      classDescription:"descripcionTexto",
+      description: "Compra tús boletos de eventospara cualquier parte del mundo.",
+      Image: "../../assets/gif/techny-email-marketing-and-newsletter.gif",
+      Button: ""
+    },
+    {
+      title: "Mejores precios",
+      classDescription:"descripcionTexto",
+      description: "Siempre gastarás menosgracias a los descuentosen tús compras.",
+      Image: "../../assets/gif/techny-dollar-coin-1.gif",
+      Button: ""
+    },
+    {
+      title: "Comienza ahora",
+      classDescription:"descripcionTexto",
+      description: "¡Empieza a disfrutar de nuestra app!",
+      Image: "../../assets/gif/techny-launching-a-profitable-startup-project.gif",
+      Button: "Comenzar"
+    }
+  ]
+
+  constructor(
+    private router: Router,
+    private storage: Storage
+    ){}
+
+  goToHome(){
+    console.log("go to home");
+    this.router.navigateByUrl('/home');
+  }
+  
+  ionViewWillEnter() {
+    console.log("Ya entraste a la intro");
+    //Vamos a guardar ne le storage que ya pasamos por la intro
+    this.storage.set('TheBox', true);
+
+  }
+
+}
+
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+// import { Router } from '@angular/ruter';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  loginForm : FormGroup;
+  validation_messages = { 
+    email: [
+      {type: 'required', message: 'Email is requerido.'},
+      {type: 'pattern', message: 'Porfavor ingresa un email valido.'}
+    ],
+    password: [
+      {type: 'required', message: 'Contraseña es requireda.'},
+      {type: 'minlength', message: 'Contraseña debe tener minimó 6 caracteres de longitud.'},
+      {type: 'maxlength', message: 'Contraseña no debe tenere más de 20 caracteres de longitud.'},
+    ]
+  }
+  loginMessage: any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    // private router: Router,
+    private navCtrl: NavController
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl(
+        '', 
+        Validators.compose([
+          Validators.required, 
+          Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")])),
+      password: new FormControl(
+        '', 
+        Validators.compose([
+          Validators.required,
+          Validators.maxLength(20), 
+          Validators.minLength(6)]))
+    })
+  }
+
+  ngOnInit() {
+  }
+
+  login(login_data : any){
+
+    this.authService.loginUser(login_data).then((response) => {
+      this.loginMessage = response;
+      this.navCtrl.navigateForward('/home');
+      // this.router.navigateByUrl('/home');
+    }).catch((error) => {
+      this.loginMessage = error;
+    });
+  }
+}
